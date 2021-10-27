8f67f47c4d99fbead2e65bfe4eaa325122847de5
gaianacheli <gaianacheli04@gmail.com>
2021-10-27 18:12:42 +0200

    esercizio completo : chiesto i numeri che ricorda e dichiarati quanti e quali indovinati

--- js/main.js
+++ js/main.js
@@ -10,8 +10,20 @@ let numPromt;
 
 for (let i = 0; i < 5; i++){
     numCasuale = Math.round(Math.random() * 100);
-    console.log(numCasuale);
     numeriRandom.push(numCasuale);
+   
 }
-
+ console.log(numeriRandom);
 alert('Memorizza questi 5 numeri: ' + numeriRandom)
+
+
+ setTimeout(function(){ 
+     for(let i = 0; i < 5; i++){
+        numPromt = parseInt(prompt('Inserisci i numeri che  ricordi'));
+            if(numPromt == numeriRandom[i]){
+                numeriUser.push(numPromt)
+            }
+     }
+     alert("Hai indovinato " + numeriUser.length + " numeri. I numeri che hai indovinato: " + numeriUser)
+    }, 3000);
+ 
